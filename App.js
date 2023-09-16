b3a673c5646046d17d188264a04ac2f27ed812bf
Raja Reddy <rajaonlyat1704@gmail.com>
2023-09-16 21:13:51 +0530

    Firt Page

--- App.js
+++ App.js
@@ -1,10 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
+import bg from './assets/bg.jpeg';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <ImageBackground source={bg} style = {styles.bg}  resizeMode='contain'>
+          <View style = {styles.circle}>
+
+          </View>
+      </ImageBackground>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +22,19 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+
+  bg:{
+    width: "100%",
+    height: "100%",
+    alignItems : "center",
+    justifyContent : "center",
+  },
+
+  circle:{
+    width: 100,
+    height: 100, 
+    backgroundColor: "white",
+    borderRadius: 50,
   },
 });
